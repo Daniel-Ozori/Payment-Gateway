f97235cf5e8cc9d2c486b465cc195cbc7572f62d
Daniel-Ozori <87254931+Daniel-Ozori@users.noreply.github.com>
2022-03-02 23:44:23 +0100

    Add files via upload

---
+++ src/Service/charge.go
@@ -0,0 +1,16 @@
+
+package Service
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	Config "stripe.com/s/src/config"
+	"stripe.com/s/src/models"
+)
+
+func SavePayment(charge *models.Charge) (err error) {
+	if err = Config.DB.Create(charge).Error; err != nil {
+		return err
+	}
+	return nil
+
+}
---
+++ src/controller/controller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+
+	Service "stripe.com/s/src/service"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/charge"
+	"stripe.com/s/src/models"
+)
+
+func Payment(c *gin.Context) {
+	var payment models.Charge
+	c.BindJSON(&payment)
+
+	//replace with your api key
+	apiKey := "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+	fmt.Println(apiKey + "asdasd")
+	stripe.Key = apiKey
+	_, err := charge.New(&stripe.ChargeParams{
+		Amount:       stripe.Int64(payment.Amount),
+		Currency:     stripe.String(string(stripe.CurrencyUSD)),
+		Description:  stripe.String(payment.ProductName),
+		Source:       &stripe.SourceParams{Token: stripe.String("tok_visa")},
+		ReceiptEmail: stripe.String(payment.ReceiptEmail)})
+
+	if err != nil {
+		c.String(http.StatusBadRequest, "Payment Unsuccessfull")
+		return
+	}
+	err1 := Service.SavePayment(&payment)
+	if err1 != nil {
+		c.String(http.StatusNotFound, "error occured")
+
+	}
+
+}
